3fd70f71aa330de3cbc4bd0d57888087308f9fd1
Dominic Gerth <d.gerth1990@googlemail.com>
2022-01-05 18:44:12 +0100

    addet the animate() function to the Clouds class to change x every 1/60 seconds by using set interval

--- models/clouds.class.js
+++ models/clouds.class.js
@@ -6,6 +6,18 @@ class Clouds extends MovableObject {
     constructor() {
         super().loadImage('img/5.Fondo/Capas/4.nubes/1.png');
         this.x = Math.random() * 1280;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            if (this.x > -1280) {
+                this.x -= 0.15;
+            }
+            else{
+                this.x = 1280;
+            }
+        }, 1000 / 60);
     }
 }
 
