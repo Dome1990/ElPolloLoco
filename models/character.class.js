329be08cdfe7b5c044d0fd6a2c089310ca7beeef
Dominic Gerth <d.gerth1990@googlemail.com>
2022-01-09 20:12:19 +0100

    addet falling animation for character

--- models/character.class.js
+++ models/character.class.js
@@ -8,18 +8,32 @@ class Character extends MovableObject {
         'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-25.png',
         'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-26.png'
     ]
+
+    IMAGES_JUMPING = [
+        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-31.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-32.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-33.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-34.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-35.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-36.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-37.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-38.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-39.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/3.Secuencia_salto/J-40.png'
+    ];
     currentImage = 0;
     world;
     speed = 8;
     otherDirection;
     x = 0;
     walking_sound = new Audio('audio/running.mp3');
-    y=0;
+    y = 0;
 
 
     constructor() {
         super().loadImage('img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
         this.animate();
         this.applyGravity();
     }
@@ -38,7 +52,7 @@ class Character extends MovableObject {
                 }
             }
             if (this.world.keyboard.LEFT) {
-                if(this.x > 0){
+                if (this.x > 0) {
                     this.x -= this.speed;
                     this.otherDirection = true;
                 }
@@ -51,19 +65,35 @@ class Character extends MovableObject {
          * walking animation
          */
         setInterval(() => {
-            if (this.world.keyboard.RIGHT) {
-                let i = this.currentImage % this.IMAGES_WALKING.length;
-                let path = this.IMAGES_WALKING[i];
+            if (this.isAbove()) {
+                let i = this.currentImage % this.IMAGES_JUMPING.length;
+                let path = this.IMAGES_JUMPING[i];
                 this.img = this.imgCache[path];
                 this.currentImage++;
             }
-            if (this.world.keyboard.LEFT) {
-                let i = this.currentImage % this.IMAGES_WALKING.length;
-                let path = this.IMAGES_WALKING[i];
-                this.img = this.imgCache[path];
-                this.currentImage++;
+            else {
+                if (this.world.keyboard.RIGHT) {
+                    let i = this.currentImage % this.IMAGES_WALKING.length;
+                    let path = this.IMAGES_WALKING[i];
+                    this.img = this.imgCache[path];
+                    this.currentImage++;
+                }
+                if (this.world.keyboard.LEFT) {
+                    let i = this.currentImage % this.IMAGES_WALKING.length;
+                    let path = this.IMAGES_WALKING[i];
+                    this.img = this.imgCache[path];
+                    this.currentImage++;
+                }
             }
         }, 60);
+        // setInterval(() => {
+        //     // if (this.isAbove()) {
+        //     //     let i = this.currentImage % this.IMAGES_JUMPING.length;
+        //     //     let path = this.IMAGES_JUMPING[i];
+        //     //     this.img = this.imgCache[path];
+        //     //     this.currentImage++;
+        //     // }
+        // }, 10)
     };
 
 }
